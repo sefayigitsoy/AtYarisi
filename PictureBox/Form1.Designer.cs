6e9b0bd68c1f84d30f74736946fbba893fc6f78f
YİĞİTSOY <YTSOY@ASY>
2022-07-25 15:30:00 +0300

    Add project files.

---
+++ PictureBox/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace PictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PictureBox.Properties.Resources.horse1;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 14);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 107);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::PictureBox.Properties.Resources.horse2;
+            this.pictureBox2.Location = new System.Drawing.Point(13, 170);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(145, 107);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::PictureBox.Properties.Resources.horse3;
+            this.pictureBox3.Location = new System.Drawing.Point(13, 326);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(145, 107);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(961, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 554);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 107);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "BAŞLAT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(1041, 572);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PictureBox/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace PictureBox
+{
+    public partial class Form1 : Form
+    {
+        Random rnd=new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            //TİMER BASLADIGI ZAMAN ATLAR RASGELE BİR SAYIYA GÖRE HAREKET EDECEK.
+            //PİCTURE BOXIN SAĞININ KONUMU LABELİN SOLUNNUN KONUMUNA DEĞDİĞİ ZAMAN OYUN BİTECEK
+            // OYUN BİTİŞİNDE 1,2,3 AT KAZANDI YAZISI EKRANDA GÖSTERİLECEK
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            SoundPlayer ses = new SoundPlayer();
+            string dizin = Application.StartupPath + "//atYarisi.wav";
+            ses.SoundLocation = dizin;
+            ses.Play();
+        }
+
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += rnd.Next(1, 25);
+            pictureBox2.Left += rnd.Next(1, 25);
+            pictureBox3.Left += rnd.Next(1, 25);
+            //SoundPlayer ses = new SoundPlayer();
+            //string dizin = Application.StartupPath + "//atYarisi.wav";
+            //ses.SoundLocation = dizin;
+            //ses.Play();
+            if (pictureBox1.Right>=label1.Left)
+            {
+                timer1.Stop();
+                //ses.Stop();
+                DialogResult dr = MessageBox.Show("1. At yarısı kazandı tekrar oynamak istermisiniz?"," At Yarışı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr==DialogResult.Yes)
+                {
+                    pictureBox1.Left = pictureBox2.Left = pictureBox3.Left = 0;
+                    timer1.Start();
+                }
+                else
+                {
+                    Close();
+                }
+            }
+            else if (pictureBox2.Right>=label1.Left)
+            {
+                timer1.Stop();
+               // ses.Stop();
+                DialogResult dr = MessageBox.Show("2. At yarısı kazandı tekrar oynamak istermisiniz?", " At Yarışı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    pictureBox1.Left = pictureBox2.Left = pictureBox3.Left = 0;
+                    timer1.Start();
+                }
+                else
+                {
+                    Close();
+                }
+            }
+            else if (pictureBox3.Right>=label1.Left)
+            {
+                timer1.Stop();
+                //ses.Stop();
+                DialogResult dr = MessageBox.Show("3. At yarısı kazandı tekrar oynamak istermisiniz?", " At Yarışı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    pictureBox1.Left = pictureBox2.Left = pictureBox3.Left = 0;
+                    timer1.Start();
+                }
+                else
+                {
+                    Close();
+                }
+            }
+        }
+    }
+}
